7ee62f3ae330e3ec509e10c6e7823a560300bf4c
William Halliburton <whalliburton@gmail.com>
2011-12-04 14:57:18 -0500

    group
    Pay attention! Another useful utility is being introduced.
    
    GROUP will group the elements of LIST into lists of N elements each.
    
    > (group '(mary joe sue sally sam tom tim) 2)
    ((MARY JOE) (SUE SALLY) (SAM TOM) (TIM))

--- classic-utilities.lisp
+++ classic-utilities.lisp
@@ -4,3 +4,16 @@
   (if (listp element)
     element
     (list element)))
+
+(defun group (list n)
+  "Group the elements of LIST into lists of N elements each."
+  (when (zerop n) (error "Groups of zero are no fun."))
+  (labels ((rec (list acc)
+             (let ((rest (nthcdr n list)))
+               (if (consp rest)
+                 (rec rest (cons
+                            (subseq list 0 n)
+                            acc))
+                 (nreverse
+                  (cons list acc))))))
+    (when list (rec list nil))))
