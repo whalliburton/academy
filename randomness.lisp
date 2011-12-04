74deb1b33aefc1ccd96be066ce19668fcdfeeed3
William Halliburton <whalliburton@gmail.com>
2011-12-03 20:54:51 -0500

    random-do
    Our first macro!
    
    > (dotimes (x 10) (random-do (princ 'hop) (princ 'skip) (princ 'jump)))
    HOPSKIPSKIPHOPJUMPSKIPSKIPJUMPSKIPSKIP
    
    The random-do above expands at read time into
    
      (CASE (RANDOM 3)
        (0 (PRINC 'HOP))
        (1 (PRINC 'SKIP))
        (2 (PRINC 'JUMP)))

--- randomness.lisp
+++ randomness.lisp
@@ -2,3 +2,10 @@
 
 (defun random-element (sequence)
   (elt sequence (random (length sequence))))
+
+(defmacro random-do (&rest statements)
+  (let ((length (length statements)))
+    `(case (random ,length)
+       ,@(loop for x from 0 to length
+               for statement in statements
+               collect `(,x ,statement)))))
