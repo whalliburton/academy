17e9add196341db516572eee5c24bd315defa338
William Halliburton <whalliburton@gmail.com>
2011-12-03 23:24:05 -0500

    random-booleans
    > (loop for el in (random-booleans (* 80 15))
                for x from 1
                do (princ (if el #\full_block #\space))
                   (when (zerop (mod x 80)) (fresh-line)))
    
      ███ ███ █  █ █    ███ █  ██ █  █ █      ███ █          ██   █ █  █████  █  █
     ██ ███       ███  █    ███████     █ ███████ ██   ███    ███ ██████   █   ██
    █  █ ███  █    █ ██   █  █ ██    ██  █ ███ █  █ ███    ██  ██ ████ ███   █  █ █
    ██ █ ███ ██ ████  █   █  █ ██ ██ █  █ ██    █████   ███ ██   █ █   ██ █ █ ███  █
    ███   █  █ ████ ██     █ █ ███ ██ ██  █   █        █ █   ███████████  █  █████
    █  ██  ███████   ██ █   █ ██ █  ███ ██  ████ ███████ ████████ █  █  ██ ██  █ █
    ████ ██ ██ █ ██████  █   █        ██ █ █ ██  █    █ ██ █ █ █ █   █ ████  █ ██ ██
     ██ ███  ██   █ █ █  ██    ██  ████  ███   █ █  █ ███  ███ █     █  █ ███████
    ████  ██ █   ██ ███  █ █   █  ██      █ ███████ ██████     █  ████ █ █████ ████
    █ █ ███  █    ██  █  █ ██  ███  █ █  █ ███ ██ ███ ██  █ █ █     █████   ███
    ██    █ ███████        ██   ██ ███  █ ██   █ ████   █    ███████ ██   ████ █ █
    ██    █ █ ██████ █ █ █ ████  █   ██  ██  ██  █   ███   █  ██ █    █ ████  █ █ ██
       █ ███  ██ █  █ █ ██   ██ █  ███ █  ███  ███   █ █   ████ ██ █  ████  █  █ █
        ██ ██    ██ █ █ █ █   █ █ ██ █ █  ██████  █ █ ██  █  █████ ███  █ █
    █ ███  █  ████ ███ █ █ █ ███   █ █ █ ███  ██  ███ █   ██ ██  ██ █ █  ███  █  █

--- randomness.lisp
+++ randomness.lisp
@@ -3,6 +3,10 @@
 (defun random-element (sequence)
   (elt sequence (random (length sequence))))
 
+(defun random-booleans (num)
+  (loop for i from 1 to num
+        collect (zerop (random 2))))
+
 (defmacro random-do (&rest statements)
   (let ((length (length statements)))
     `(case (random ,length)
