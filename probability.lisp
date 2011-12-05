72b6a9ca1d0ce644237277496e839ac411f0bba0
William Halliburton <whalliburton@gmail.com>
2011-12-05 10:42:32 -0500

    visualize-chance
    > (visualize-chance)
    
    █▄██████████▀█▄██ ▄██▄█▀███▄▀▄▀▄█▀▄ ▄▄█▄█▄▄▄█▄█ ▄▀▄ ▀ █   █           ▄
    █████████▀▀▄██▀▀▄██▀▀▄███▀██▀▄▄▀▀▀▄▄▀ █▀▀▀▀▀▄▀█▀ ▀ █ ▀▀▀▄ ▄▄ ▄ ▄   ▀▀
    ███▀██████▀███████▄████▄██▄███▄▀▀  ██▄█▀▄█  ▀ ▄    ▄▄ ▀ ▀  ▄   ▀       ▄  ▀
    █████████████▄███ █ █ ████▄██ ▀█▄ █▄▀  █▀█ ▄    ▄▄▄█   ▀▄▄█▄
    █████████████▄███████▄██ ▀█ ▄▀ ▄█▀▀▀███▄█▀▄▄▄▄▄ ▄   ▀  ▀ ▀█   ▄▄   ▄
    ████████▄▀██████▀████▄▀▀ ▄▀█████ ▄█▄▄▀▄█▀ ▀▀██▄▀██ ▀▀▀ ▄ ▀▀  ▄▀     ▄    ▄
    ████▄████▀▄█▄█▀█████▀▀ ██ █▄ ▀██▄▀█▀█▄▄███▄███▀▄▀▀     ▀ ▄  ▄▀▀▀▀▀      ▄
    ████████▄▄▄███▀▀ █▄███▀▀█▄▀ █▄▀▀▄ ▀▀▄▀█▄ ▀█▄█ ▄ ▀▄  ▄▀ ▄▄ ▀  ▀  █▀ ▀▄ ▀▄
    █████▄███▀██▄▀██▀▄██▀███ ▀██▄█▀█▄█▄█▀█▀▀▀ ▄▄▄▀▀ █  ▀  ▄▀ ▄     ▄▄▄     ▀
    ██████████ ███▀███ ████ ███▀▀█▀▄▀█▀ █▄▄▀███   ▄█▀▄▀▄▄ ▄   ▀     ▀▄    ▀
    ███████████████▀▀▀███ █▀ ▀███▄██▄█▀ ███▄▄▀ █  ▄▀▄  ▀    ▄▀     ▄▄ ▄ ▀  ▄
    ███████████▀███▀█ ▄█▄▀▄██▄██ █▄██▄▀█▀ ██▀   ▄▄  ▀█ ▄  ▄  ▄     ▀  ▄ ▀ ▀▄ ▀
    ██████████████████▀█▄▄█▀▄█▀█ ▀█ ▀▄▄▄▄█▄▄▀▀ ▄ ▄▄█▀ ▄▄  ▀ ▄█▄    ▀  ▀      ▀
    ███████████▀██▄█ ▄█▄█▀██▄███████ ▀ ▄█ █▄█▀▀▀█ ██▄  ▀▄▀ ▄  █  ▄▀▀    ▀▄
    ███████ ███▄████████ █▀▀▀█▄█▄██▄▀▄▄ ███▄▀█  ▄▄█▄   ▄▄         █▄▀   ▄       ▀
    ██████▄███▀▄█████▄██▀ ▄███ █▀▄█▄█▀█▄▀█▀▀▀▄█ ▄▄▀  ▀▀▀▄▄▄▀ ▄▀  ▄   ▀

--- probability.lisp
+++ probability.lisp
@@ -2,4 +2,11 @@
 
 (defun visualize-one-in-chance (&optional (chance 3))
   "Show a 32x32 bitmap with pixels on with a one in CHANCE probability."
-  (draw-bitmap-from-list (random-booleans (* 32 32) chance) 32))
+  (draw-from-list (random-booleans (* 32 32) chance) 32))
+
+(defun visualize-chance (&optional (steps 80))
+  "Show a bitmap with each columns pixels with decreasing probability."
+  (draw (make-bitmap steps 32
+                     (loop for y from 1 to 32
+                           collect (loop for i from 1 to steps
+                                         collect (>= (random steps) i))))))
