63143c5a2841d256d47fe179db07e3c867c1ead1
William Halliburton <whalliburton@gmail.com>
2011-12-06 11:40:38 -0500

    print-table
    > (loop for args in '(nil (:gap "$$") (:align :left))
            do (apply #'print-table
                      (loop for x from 1 to 6
                            collect
                               (loop for x from 1 to 6
                                     collect (random-do (random 100000)
                                                        (random-element '(bull bear)))))
                      args)
               (terpri))
    
     BEAR   BEAR   BULL   BULL  87984  84947
     BEAR  69563  44895  29735   BEAR  85831
    72832   BULL   BULL  77389  82439  86364
     BULL   BULL   BEAR   BULL   BULL   BULL
     BULL   BEAR   BEAR  73444   BEAR  43194
    87523  33990  24603   BEAR  63915   BULL
    
     BEAR$$67880$$64999$$ BEAR$$66444$$88571
    29536$$ BEAR$$28664$$ BEAR$$96143$$ BULL
     BEAR$$70373$$82057$$85997$$99603$$ 3351
    47959$$ BEAR$$ BEAR$$73129$$ 8184$$ BULL
    43649$$97104$$ BEAR$$ BEAR$$ BULL$$ BULL
     BULL$$ BULL$$ BEAR$$68957$$ BEAR$$ BEAR
    
    65931  8108   72867  BULL   68808  34300
    7998   87740  BEAR   BULL   BEAR   15545
    55447  86670  BEAR   BULL   BULL   BULL
    79437  BEAR   BEAR   43880  23451  BEAR
    BULL   9323   75928  52182  BEAR   2799
    BEAR   80488  BULL   65464  BULL   58910
    
    Ugly, ain't it!

---
+++ pretty-printing.lisp
@@ -0,0 +1,27 @@
+(in-package :academy)
+
+(defun rotate-rows-to-columns (rows)
+  (loop for remaining = rows then (mapcar #'cdr remaining)
+        while (not (every #'null remaining))
+        collect (mapcar #'car remaining)))
+
+(defun maximize-length (list &key (key #'identity))
+  (loop for element in list maximizing (length (funcall key element))))
+
+(defun pad-list (list length &optional (pad-element nil))
+  (loop for el on list
+        for x from 1
+        do (when (null (cdr el))
+             (setf (cdr el) (make-list (- length x) :initial-element pad-element))
+             (return list))))
+
+(defun print-table (rows &key (gap "  ") (align :right))
+  (loop with control-string =
+           (format nil
+                   (concatenate
+                    'string "~{~~~D" (ecase align (:right "@") (:left "")) "A~^" gap "~}~%")
+                   (mapcar (lambda (row) (maximize-length row :key #'princ-to-string))
+                           (rotate-rows-to-columns rows)))
+        for row in (let ((max-row-length (apply #'max (mapcar #'length rows))))
+                     (mapcar (lambda (row) (pad-list row max-row-length "")) rows))
+        do (apply #'format t control-string row)))
