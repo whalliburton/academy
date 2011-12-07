e40b0c2a12688d348be2c8932d37ce691cfb46e1
William Halliburton <whalliburton@gmail.com>
2011-12-06 21:50:18 -0500

    sbcl-nuts-and-bolts
    > (princ (run-program-to-string "echo" '("Is" "anyone" "out" "there" "?")))
    
    Is anyone out there ?
    
    ;;; With the classic bsdgames installed...
    
    > (princ (run-program-to-string "morse" '("code")))
    
     daw dit daw dit
     daw daw daw
     daw dit dit
     dit
    
    > (princ (run-program-to-string "fortune" nil))
    
    You're currently going through a difficult transition period called "Life."

---
+++ sbcl-nuts-and-bolts.lisp
@@ -0,0 +1,24 @@
+(in-package :academy)
+
+;;; SBCL specific utilities.
+
+;;; Attention non-SBCL hackers! Compatibility code for your lisp of choice would be
+;;; happily integrated here! For the betterment of lisp and the furtherment of humanity's
+;;; hopefully everlasting search for objective truths.
+
+(defun ensure-process-success (process &key (expected-exit-code 0)
+                                            (expected-status :exited))
+  (let ((status (sb-ext:process-status process))
+        (exit-code (sb-ext:process-exit-code process)))
+    (unless (eql status expected-status)
+      (error "process-failure-status"))
+    (unless (eql exit-code expected-exit-code)
+      (error "process-failure-exit"))
+    process))
+
+(defun run-program-to-string (program args)
+  "Run system program PROGRAM with arguments ARGS.
+Returns all the output of standard output and standard error as a string."
+  (with-output-to-string (stream)
+    (ensure-process-success
+     (sb-ext:run-program program args :output stream :error stream :search t))))
