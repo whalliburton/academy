7394ca75c53441590c9205f75b172301e620b220
William Halliburton <whalliburton@gmail.com>
2011-12-07 15:20:51 -0500

    count-unique
    > (count-unique (random-booleans 100))  ; Your mileage may vary.
    ((T 44) (NIL 56))
    
    > (count-unique (random-booleans 100 3))
    ((T 30) (NIL 70))
    
    > (count-unique (collect 100 (random 10)))
    ((5 9) (7 15) (8 6) (2 13) (4 15) (0 11) (6 10) (9 10) (3 9) (1 2))
    
    > (reduce #'+ (mapcar #'second *))
    100
    
    > (count-unique (collect 20 (random-element '(fe fi fo fum))))
    ((FO 5) (FI 5) (FE 3) (FUM 7))

--- lists.lisp
+++ lists.lisp
@@ -5,3 +5,15 @@
   (let ((counter (gensym)))
     `(loop for ,counter from 1 to ,number
            collect (progn ,@body))))
+
+(defun count-unique (list &optional (test #'equal))
+  "Return as list of (NAME COUNT) of unique names from LIST and the number of occurances
+in the list of this unique name."
+  (let ((hash (make-hash-table :test test)))
+    (loop for element in list
+          do (if (gethash element hash)
+               (incf (gethash element hash))
+               (setf (gethash element hash) 1)))
+    (loop for name being each hash-key of hash
+          for count being each hash-value of hash
+          collect (list name count))))
