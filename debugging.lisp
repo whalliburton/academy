626a224d1be56374c02a55709d2bdca6a2c081e5
William Halliburton <whalliburton@gmail.com>
2011-12-11 22:02:56 -0500

    breakout
    BREAKOUT is like BUGOUT, but signals a BREAK with the select variables
    being the arguments to this BREAK. This allows for easily inserting
    breakpoints that give readouts of variables and also further access
    from within the SLIME debugger backtrace to these variables.

--- debugging.lisp
+++ debugging.lisp
@@ -1,12 +1,21 @@
 (in-package :academy)
 
-(defmacro bugout (&rest vars)
-  "Print VARS, for debugging."
-  `(format t ,(with-output-to-string (s)
-                (write-string "~%>>>" s)
-                (loop for var in vars
-                      do (write-string "  " s)
-                         (prin1 var s)
-                         (unless (keywordp var) (write-string " ~S" s)))
-                (write-string "~2%" s))
-           ,@(remove-if #'keywordp vars)))
+(flet ((generate-control-string (vars)
+         (with-output-to-string (s)
+           (loop for var on vars
+                 do (prin1 (car var) s)
+                    (unless (keywordp (car var)) (write-string " ~S" s))
+                    (when (cdr var) (format s "  "))))))
+
+  (defmacro bugout (&rest vars)
+    "Print VARS, for debugging."
+    `(format t ,(with-output-to-string (s)
+                  (write-string "~%>>>  " s)
+                  (write-string (generate-control-string vars) s)
+                  (write-string "~2%" s))
+             ,@(remove-if #'keywordp vars)))
+
+  (defmacro breakout (&rest vars)
+    "Break with VARS, for debugging."
+    `(break ,(generate-control-string vars)
+            ,@(remove-if #'keywordp vars))))
