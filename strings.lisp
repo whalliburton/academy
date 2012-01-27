e4e23364cd4e4a844440d37aab93e60eef500c94
William Halliburton <whalliburton@gmail.com>
2012-01-27 14:51:33 -0800

    split-string
    > (split-string #\space "We love space.")
    ("We" "love" "space.")

--- strings.lisp
+++ strings.lisp
@@ -1,8 +1,19 @@
 (in-package :academy)
 
 (defun split-string-into-lines (string)
-  "Return a list containing the lines from STRING split on the end of each line."
+  "Return a list containing the lines from STRING split on the end of each line"
   (with-input-from-string (stream string)
-    (loop for line = (read-line stream nil )
+    (loop for line = (read-line stream nil)
           while line
           collect line)))
+
+(defun split-string (delimiter string)
+  "Return a list containing the strings from STRING split on DELIMITER."
+  (loop with rtn
+        with acc
+        for char across string
+        if (char= delimiter char)
+        do (push (coerce (nreverse acc) 'string) rtn)
+           (setf acc nil)
+        else do (push char acc)
+        finally (return (nreverse (cons (coerce (nreverse acc) 'string) rtn)))))
